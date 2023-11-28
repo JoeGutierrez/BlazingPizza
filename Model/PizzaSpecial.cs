a74658f2457b49d522d3b6b154f896a9bfecba07
Gutierrez <JGutierrez@newportbeachca.gov>
2023-11-28 14:55:07 -0800

    Initial commit.

---
+++ Model/PizzaSpecial.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazingPizza
+{
+    /// <summary>
+    /// Represents a pre-configured template for a pizza a user can order
+    /// </summary>
+    public class PizzaSpecial
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal BasePrice { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string GetFormattedBasePrice() => BasePrice.ToString("0.00"); // Lambda expressions and anonymous functions https://stackoverflow.com/questions/4829054/what-does-this-c-sharp-code-with-an-arrow-an-equal-sign-and-greater-than#52426208 https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/operators/lambda-expressions Added: 11/28/2023.
+    }
+}
